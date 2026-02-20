bf2c5abf3909035a97f68d6fb2830d411c11213c
issac1441 <thetimmy0524@gmail.com>
2026-02-20 17:41:54 +0800

    chore: rm types for reducing complexity

--- dccox/types.py
+++ dccox/types.py
@@ -5,19 +5,10 @@ from __future__ import annotations
 from typing import Annotated
 
 import numpy as np
-import pandas as pd
 from pydantic import AfterValidator
 
 
 def _validate_1d(v: np.ndarray) -> np.ndarray:
-    if v.ndim != 1:
-        raise ValueError(f"Expected 1D array, got {v.ndim}D")
-    return v
-
-
-def _validate_1d_like(v: ArrayLike1D) -> np.ndarray:
-    if isinstance(v, (pd.Series, list)):
-        v = np.array(v)
     if v.ndim != 1:
         raise ValueError(f"Expected 1D array, got {v.ndim}D")
     return v
@@ -31,12 +22,3 @@ def _validate_2d(v: np.ndarray) -> np.ndarray:
 
 Array1D = Annotated[np.ndarray, AfterValidator(_validate_1d)]
 Array2D = Annotated[np.ndarray, AfterValidator(_validate_2d)]
-
-ArrayLike1D = Annotated[
-    Array1D | pd.Series | list[float], AfterValidator(_validate_1d_like)
-]
-ArrayLike2D = Annotated[
-    Array2D | pd.DataFrame | list[list[float]] | list[Array1D],
-    AfterValidator(_validate_2d),
-]
-ArrayLike = ArrayLike1D | ArrayLike2D
